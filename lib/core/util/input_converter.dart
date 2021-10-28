09d773f982ed4c48c6d2da56a7d8142cd1d9f793
bnaky <ykanbour@gmail.com>
2021-10-29 00:49:16 +0300

    sucessfully imported package from git repo

--- lib/core/util/input_converter.dart
+++ lib/core/util/input_converter.dart
@@ -1,12 +1,14 @@
 import 'package:dartz/dartz.dart';
+import 'package:exporting_package/exporting_package.dart';
 
 import '../error/failures.dart';
 
 class InputConverter {
   Either<Failure, int> stringToUnsignedInteger(String str) {
     try {
-      final integer = int.parse(str);
+      var integer = int.parse(str);
       if (integer < 0) throw const FormatException();
+      integer = Calculator().addOne(integer);
       return Right(integer);
     } on FormatException {
       return Left(InvalidInputFailure());
